e4fdf53077c12b202d0abd158cac4a0cd99df7d9
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2023-05-30 21:10:49 +0700

    init

---
+++ config/plugins.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  documentation: {
+    enabled: true,
+    config: {
+      servers: [{ url: 'https://bangkit-capstone-strapi.nabiel.my.id/api', description: 'Development server' }],
+    }
+  }
+}
